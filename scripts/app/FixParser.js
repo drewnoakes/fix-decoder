9ccf78fe6871035117560e46eb47ad6763114f18
Julien <whatthefrog@ymail.com>
2015-03-15 20:37:59 +0000

    Validation: Calculate the FIX messages body length and compare it to the one given in FIX field BodyLength(9)
    Also put correct values in the examples

--- scripts/app/FixParser.js
+++ scripts/app/FixParser.js
@@ -6,6 +6,7 @@ define(
     {
         var FIELD_CHECKSUM = 10,
             BEGIN_STRING = 8,
+            BODY_LENGTH = 9,
             MSG_TYPE = 35,
             SENDER_COMP_ID = 49,
             TARGET_COMP_ID = 56;
@@ -57,6 +58,7 @@ define(
                     fieldId: fieldId,
                     value: value,
                     field: field,
+                    raw: fieldId + "=" + value + "\01",
                     decodedValue: decodedValue,
                     classes: classes.join(' ')
                 });
@@ -113,6 +115,47 @@ define(
                         }
 
                         message.fields.push(field);
+                    }
+                }
+            );
+
+            // BodyLength(9) verification
+            _.each(
+                messages,
+                function(message)
+                {
+                    var field_bodylength = undefined;
+                    length = 0;
+
+                    _.each(
+                        message.fields,
+                        function(field)
+                        {
+                            if ( field.fieldId == BODY_LENGTH ) {
+                                field_bodylength = field;
+                                return;
+                            }
+
+                            // Some fields are not part of the FIX message body, skip them
+                            if ( field.fieldId == BEGIN_STRING || field.fieldId == FIELD_CHECKSUM ) {
+                                return;
+                            }
+
+                            length += field.raw.length;
+                        }
+                    );
+
+                    if ( ! field_bodylength ) {
+                        return;
+                    }
+
+                    if ( field_bodylength.value == length ) {
+                        field_bodylength.classes += ' valid';
+                        field_bodylength.decodedValue = 'Valid';
+                    }
+                    else {
+                        field_bodylength.classes += ' invalid';
+                        field_bodylength.decodedValue = '/!\\ Invalid (expected ' + length + ')';
                     }
                 }
             );
